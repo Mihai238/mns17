63e4890184e530673d6747dd4ca71958b185eac1
Mihai Lepadat <mihai.lepadat@irian.at>
2016-11-30 10:58:15 +0100

    use ButterKnife for injecting views

--- AndroidCellInfos/app/src/main/java/at/ac/tuwien/at/androidcellinfos/MainActivity.java
+++ AndroidCellInfos/app/src/main/java/at/ac/tuwien/at/androidcellinfos/MainActivity.java
@@ -7,18 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
