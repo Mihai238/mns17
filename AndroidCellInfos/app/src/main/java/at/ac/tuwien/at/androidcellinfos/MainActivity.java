0139515db0a628ed221d45bb7ad5321bc8994c30
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-12-03 12:14:15 +0100

    fix permission for Android version >= 6

--- AndroidCellInfos/app/src/main/java/at/ac/tuwien/at/androidcellinfos/MainActivity.java
+++ AndroidCellInfos/app/src/main/java/at/ac/tuwien/at/androidcellinfos/MainActivity.java
@@ -1,7 +1,12 @@
 package at.ac.tuwien.at.androidcellinfos;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -26,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         initializeTabs();
+
+        askPermissions();
     }
 
     private void initializeTabs() {
@@ -40,5 +47,13 @@ public class MainActivity extends AppCompatActivity {
 
         mViewPager.setAdapter(mSectionsPagerAdapter);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+    private void askPermissions() {
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
+        }
     }
 }
