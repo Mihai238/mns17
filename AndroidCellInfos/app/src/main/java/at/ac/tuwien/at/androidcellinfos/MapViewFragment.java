38dd2e7ab6eef134aa453133d8a7c6e08e62e77f
Michael Langhammer <e1125605@student.tuwien.ac.at>
2016-12-05 22:47:39 +0100

    Added MapView

--- AndroidCellInfos/app/src/main/java/at/ac/tuwien/at/androidcellinfos/MapViewFragment.java
+++ AndroidCellInfos/app/src/main/java/at/ac/tuwien/at/androidcellinfos/MapViewFragment.java
@@ -9,16 +9,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import at.ac.tuwien.at.androidcellinfos.service.CellModel;
 import at.ac.tuwien.at.androidcellinfos.service.CellModelWatcher;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class MapViewFragment extends Fragment {
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
 
     private static final String TAG = "MapViewFragment";
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     private CellModelWatcher cellModelWatcher;
+    MapView mapView;
 
     public static MapViewFragment newInstance() {
         return new MapViewFragment();
@@ -33,6 +43,15 @@ public class MapViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map_view, container, false);
 
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mapView = (MapView) rootView.findViewById(R.id.map_view);
+        mapView.onCreate(mapViewBundle);
+
+        mapView.getMapAsync(this);
+
         return rootView;
     }
 
@@ -40,13 +59,12 @@ public class MapViewFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        TelephonyManager telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-        cellModelWatcher = new CellModelWatcher(telephonyManager);
-
-        cellModelWatcher.watch()
-                .subscribeOn(Schedulers.computation())
-                .observeOn(AndroidSchedulers.mainThread())
-                .forEach(cellInfo -> Log.i(TAG, "Got: " + cellInfo));
+        View rootview = getView();
+        if (rootview != null && this.mapView == null) {
+            this.mapView = (MapView) rootview.findViewById(R.id.map_view);
+        } else {
+            Log.e(TAG, "Unable to detect root view of Fragment");
+        }
     }
 
     @Override
@@ -54,5 +72,35 @@ public class MapViewFragment extends Fragment {
         super.onPause();
 
         cellModelWatcher.close();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        TelephonyManager telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+        cellModelWatcher = new CellModelWatcher(telephonyManager);
+
+        cellModelWatcher.watch()
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .forEach(cellInfo -> {
+                    Log.i(TAG, "Found " + cellInfo.size() + " cells");
+
+                    // TODO add/remove marker
+                });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
     }
 }
