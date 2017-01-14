069cc76b23e226b80d4eb13330b3b1bfb70675b3
Jakob Abfalter <Jakobabfalter@gmail.com>
2017-01-14 16:18:25 +0100

    Basic Setup Java 8 App

---
+++ Java8Example/app/src/main/java/at/ac/tuwien/mns17/java8example/MainActivity.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.mns17.java8example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
