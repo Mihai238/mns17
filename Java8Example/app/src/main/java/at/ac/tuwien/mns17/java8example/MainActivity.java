f29981b87a0c4b5a2a5ab83dfeb8002a463fd89a
Jakob <jakobabfalter@gmail.com>
2017-01-15 14:38:32 +0100

    Implemented all required Java8 Examples

--- Java8Example/app/src/main/java/at/ac/tuwien/mns17/java8example/MainActivity.java
+++ Java8Example/app/src/main/java/at/ac/tuwien/mns17/java8example/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import at.ac.tuwien.mns17.java8example.models.User;
@@ -33,13 +34,33 @@ public class MainActivity extends AppCompatActivity {
          * The Java8 Stream API makes manipulating lists much easier with less code required.
          * The following code samples takes a list of Users, extracts all users from Austria
          * with ages 18 and higher and extracts their usernames into a new list
+         * Not that this sample already uses Lambda Expressions which will be explained further in a
+         * following code sample
          */
 
         List<String> atUsersOver18 = sampleUsers.stream()
                                                 .filter(u -> u.getAge() >= 18)
                                                 .filter(u -> u.getCountry().equals("AT"))
-                                                .map(User::getUsername)
+                                                .map(user -> user.getUsername())
                                                 .collect(Collectors.toList());
+
+        /**
+         * Lambda Expressions:
+         * Now we do the same thing we did in the Stream API example again using a Lambda Expression.
+         * We use the Predicate Interface which has exactly one abstract Method called test
+         * Instead of creating an Anonymous class implementing the interface and method we use a Lambda expression
+         * to implement the method.
+         * This not only simplifies the code but also improves efficiency since lambdas are implemented
+         * differently and more efficiently than Anonymous inner classes.
+         */
+        atUsersOver18 = getUsernamesOfSpecificUsers(sampleUsers, u -> u.getAge() >= 18 && u.getCountry().equals("AT"));
+
+        /**
+         * Method References:
+         * Method Referneces are syntactic improvements to Lambda Expressions if in the Lambda Expressions only a method
+         * is called
+         */
+        atUsersOver18 = getUsernamesOfSpecificUsersMethodReference(sampleUsers, u -> u.getAge() >= 18 && u.getCountry().equals("AT"));
 
         /**
          * Default Interface Methods:
@@ -52,6 +73,19 @@ public class MainActivity extends AppCompatActivity {
         ServiceCheck uptimeCheck = new WebsiteUptimeCheck("http://koreanbuilds.net");
         boolean siteUp = uptimeCheck.testConnection();
 
+    }
 
+    private List<String> getUsernamesOfSpecificUsers(List<User> users, Predicate<User> tester) {
+        return users.stream()
+                .filter(u -> tester.test(u))
+                .map(u -> u.getUsername())
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getUsernamesOfSpecificUsersMethodReference(List<User> users, Predicate<User> tester) {
+        return users.stream()
+                .filter(tester::test)
+                .map(User::getUsername)
+                .collect(Collectors.toList());
     }
 }
